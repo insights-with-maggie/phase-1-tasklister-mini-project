e9764b829482a538cb4d6f1c42d098e0927a955a
margret mdungu <kasichanamargretmdungu59@gmail.com>
2025-12-28 16:05:57 +0300

    Added and created a to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+// Select the form
+const form = document.querySelector("#create-task-form");
+
+// Add submit event listener
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Get input value using name (matches test)
+  const task =
+    event.currentTarget.elements["new-task-description"].value;
+
+  buildToDo(task);
+});
+
+// Function to build task
+function buildToDo(task) {
+  const taskList = document.querySelector("#tasks");
+
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  taskList.appendChild(li);
+}
